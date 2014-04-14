ec71842c805409a7e027b3d6a860a1542e54c791
Inez Korczyński <inez@wikia-inc.com>
2014-04-14 00:49:13 -0700

    Basic click handling for bottom menu

--- stuffapp/src/com/stuff/stuffapp/MainActivity.java
+++ stuffapp/src/com/stuff/stuffapp/MainActivity.java
@@ -1,15 +1,21 @@
 package com.stuff.stuffapp;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
+	public static String LOG_TAG = "MainActivity";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		//ImageView iv_home = (ImageView) findViewById(R.id.iv_home);
 	}
 
 	@Override
@@ -17,6 +23,27 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void onMenuItemClick(View view) {
+		switch ( view.getId() ) {
+			case R.id.iv_home:
+				Log.d(LOG_TAG, "Home clicked");
+				break;
+			case R.id.iv_search:
+				Log.d(LOG_TAG, "Search clicked");
+				break;
+			case R.id.iv_add:
+				Log.d(LOG_TAG, "Add clicked");
+				break;
+			case R.id.iv_message:
+				Log.d(LOG_TAG, "Message clicked");
+				break;
+			case R.id.iv_profile:
+				Log.d(LOG_TAG, "Profile clicked");
+				break;
+		}
+		
 	}
 
 }
