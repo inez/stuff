24288c97f049a8dc67197a4ddd75ebf18a07bed8
Justin Kuo <calvin-n-hobbes@bazinga.fake>
2014-04-20 15:46:45 -0700

    Added ViewPager on PROFILE to swipe between "About Me" and "My Items"; still missing tab indicators

---
+++ stuffapp/src/com/stuff/stuffapp/fragments/MyItemsFragment.java
@@ -0,0 +1,43 @@
+package com.stuff.stuffapp.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.stuff.stuffapp.R;
+import com.stuff.stuffapp.adapters.MyItemsAdapter;
+
+public class MyItemsFragment extends Fragment {
+    private static final String TAG = "MyItemsFragment";
+
+    private ListView lvMyItems;
+    private MyItemsAdapter adapter;
+
+    public static final MyItemsFragment newInstance() {
+        return new MyItemsFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView");
+        View view = inflater.inflate(R.layout.fragment_my_items, container, false);
+
+        lvMyItems = (ListView) view.findViewById(R.id.lv_my_items);
+
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        if ( null == adapter ) {
+            adapter = new MyItemsAdapter(getActivity());
+        }
+        lvMyItems.setAdapter(adapter);
+    }
+}
--- stuffapp/src/com/stuff/stuffapp/fragments/ProfileFragment.java
+++ stuffapp/src/com/stuff/stuffapp/fragments/ProfileFragment.java
@@ -1,28 +1,27 @@
 package com.stuff.stuffapp.fragments;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
-import android.widget.TextView;
 
-import com.facebook.widget.ProfilePictureView;
-import com.parse.ParseUser;
 import com.stuff.stuffapp.R;
-import com.stuff.stuffapp.adapters.MyItemsAdapter;
 
 public class ProfileFragment extends Fragment {
 	private static final String TAG = "ProfileFragment";
 	
 	private View view;
-	private ListView lv_myItems;
-	private MyItemsAdapter adapter;
+    private ViewPager viewPager;
+	private ProfilePagerAdapter pagerAdapter;
+
+	// temporary
+	private AboutMeFragment firstProfileFrag;
+	private MyItemsFragment secondProfileFrag;
 
 	public static ProfileFragment newInstance() {
 		ProfileFragment fragment = new ProfileFragment();
@@ -34,27 +33,10 @@ public class ProfileFragment extends Fragment {
 		Log.d(TAG, "onCreateView");
         view = inflater.inflate(R.layout.fragment_profile, container, false);
 
-        lv_myItems = (ListView) view.findViewById(R.id.lv_myItems);
-        
-        ParseUser currentUser = ParseUser.getCurrentUser();
-        TextView tv_name = (TextView) view.findViewById(R.id.tv_name);
-		if (currentUser.get("profile") != null) {
-			JSONObject userProfile = currentUser.getJSONObject("profile");
-			try {
-				ProfilePictureView ppv = (ProfilePictureView) view.findViewById(R.id.profile_picture_view);
-				ppv.setProfileId(userProfile.get("facebookId").toString());
-				
-				tv_name.setText(userProfile.getString("name"));
-				
-				TextView tv_location = (TextView) view.findViewById(R.id.tv_location);
-				tv_location.setText(userProfile.getString("location"));
-			} catch (JSONException e) {
-				Log.d(TAG, "Error parsing saved user data.");
-			}
-		}
-		else {
-		    tv_name.setText("User information unavailable");
-		}
+        viewPager = (ViewPager) view.findViewById(R.id.profile_view_pager);
+        // temporary
+        firstProfileFrag = new AboutMeFragment();
+        secondProfileFrag = new MyItemsFragment();
 
         return view;
     }
@@ -63,9 +45,30 @@ public class ProfileFragment extends Fragment {
 	public void onActivityCreated(Bundle savedInstanceState) {
 	    super.onActivityCreated(savedInstanceState);
 
-	    if ( null == adapter ) {
-	        adapter = new MyItemsAdapter(getActivity());
-	    }
-        lv_myItems.setAdapter(adapter);         
+        pagerAdapter = new ProfilePagerAdapter(getActivity().getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
+	}
+
+	/**
+	 * Internal adapter class for ViewPager 
+	 */
+	public class ProfilePagerAdapter extends FragmentPagerAdapter {
+        public ProfilePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            if ( position == 0 )
+                return firstProfileFrag;
+            else if ( position == 1 )
+                return secondProfileFrag;
+            else return firstProfileFrag;
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
 	}
 }
