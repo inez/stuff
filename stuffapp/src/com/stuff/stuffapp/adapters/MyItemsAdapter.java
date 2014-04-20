9da0da78d5521600c9f9cb45a44b0fff95aafe49
Justin Kuo <calvin-n-hobbes@bazinga.fake>
2014-04-20 01:15:34 -0700

    Added basic "My Items" list to Profile fragment

---
+++ stuffapp/src/com/stuff/stuffapp/adapters/MyItemsAdapter.java
@@ -0,0 +1,66 @@
+package com.stuff.stuffapp.adapters;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.assist.ImageLoadingListener;
+import com.parse.ParseQuery;
+import com.parse.ParseQueryAdapter;
+import com.parse.ParseUser;
+import com.stuff.stuffapp.R;
+import com.stuff.stuffapp.models.Item;
+
+public class MyItemsAdapter extends ParseQueryAdapter<Item> {
+    public MyItemsAdapter(Context context) {
+        super(context, new ParseQueryAdapter.QueryFactory<Item>() {
+            @Override
+            public ParseQuery<Item> create() {
+                ParseQuery<Item> query = new ParseQuery<Item>(Item.class);
+                query.whereEqualTo("owner", ParseUser.getCurrentUser());
+                query.orderByAscending("createdAt");
+                return query;
+            }
+        });
+    }
+
+    @Override
+    public View getItemView(Item item, View view, ViewGroup parent) {
+        if ( null == view ) {
+            view = View.inflate(getContext(), R.layout.item_list_profile, null);
+        }
+
+        final ImageView iv_myItem = (ImageView) view.findViewById(R.id.iv_my_item);
+        TextView tv_myItemName = (TextView) view.findViewById(R.id.tv_my_item_name);
+        TextView tv_myItemDesc = (TextView) view.findViewById(R.id.tv_my_item_desc);
+
+        ImageLoader imageLoader = ImageLoader.getInstance();
+        imageLoader.displayImage(item.getPhotoFile200().getUrl(), iv_myItem, new ImageLoadingListener() {
+            @Override
+            public void onLoadingStarted(String imageUri, View view) {
+            }
+            
+            @Override
+            public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+            }
+            
+            @Override
+            public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                iv_myItem.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onLoadingCancelled(String imageUri, View view) {
+            }
+        });
+        tv_myItemName.setText(item.getName());
+        tv_myItemDesc.setText(item.getDescription());
+
+        return view;
+    }
+}
--- stuffapp/src/com/stuff/stuffapp/fragments/ProfileFragment.java
+++ stuffapp/src/com/stuff/stuffapp/fragments/ProfileFragment.java
@@ -9,17 +9,20 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.facebook.widget.ProfilePictureView;
 import com.parse.ParseUser;
 import com.stuff.stuffapp.R;
+import com.stuff.stuffapp.adapters.MyItemsAdapter;
 
 public class ProfileFragment extends Fragment {
-
-	private static String TAG = "ProfileFragment";
+	private static final String TAG = "ProfileFragment";
 	
 	private View view;
+	private ListView lv_myItems;
+	private MyItemsAdapter adapter;
 
 	public static ProfileFragment newInstance() {
 		ProfileFragment fragment = new ProfileFragment();
@@ -30,15 +33,17 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		Log.d(TAG, "onCreateView");
         view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        lv_myItems = (ListView) view.findViewById(R.id.lv_myItems);
         
         ParseUser currentUser = ParseUser.getCurrentUser();
+        TextView tv_name = (TextView) view.findViewById(R.id.tv_name);
 		if (currentUser.get("profile") != null) {
 			JSONObject userProfile = currentUser.getJSONObject("profile");
 			try {
 				ProfilePictureView ppv = (ProfilePictureView) view.findViewById(R.id.profile_picture_view);
 				ppv.setProfileId(userProfile.get("facebookId").toString());
 				
-				TextView tv_name = (TextView) view.findViewById(R.id.tv_name);
 				tv_name.setText(userProfile.getString("name"));
 				
 				TextView tv_location = (TextView) view.findViewById(R.id.tv_location);
@@ -47,8 +52,20 @@ public class ProfileFragment extends Fragment {
 				Log.d(TAG, "Error parsing saved user data.");
 			}
 		}
+		else {
+		    tv_name.setText("User information unavailable");
+		}
 
-		return view;
+        return view;
     }
 
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+	    super.onActivityCreated(savedInstanceState);
+
+	    if ( null == adapter ) {
+	        adapter = new MyItemsAdapter(getActivity());
+	    }
+        lv_myItems.setAdapter(adapter);         
+	}
 }
