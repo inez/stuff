509c9819abeab43a4498c08ce4e6b9dbf51baaf4
Justin Kuo <calvin-n-hobbes@bazinga.fake>
2014-04-20 22:02:34 -0700

    Work-around for ViewPager display bug in Profile

--- stuffapp/src/com/stuff/stuffapp/fragments/ProfileFragment.java
+++ stuffapp/src/com/stuff/stuffapp/fragments/ProfileFragment.java
@@ -1,20 +1,53 @@
 package com.stuff.stuffapp.fragments;
 
+import java.lang.reflect.Field;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.stuff.stuffapp.R;
+//import com.viewpagerindicator.UnderlinePageIndicator;
 
 public class ProfileFragment extends Fragment {
 	private static final String TAG = "ProfileFragment";
-	
+
+	// begin bug fix for ViewPager within fragment
+	// http://stackoverflow.com/questions/14929907/causing-a-java-illegalstateexception-error-no-activity-only-when-navigating-to
+	// https://code.google.com/p/android/issues/detail?id=42601
+	private static final Field sChildFragmentManagerField;
+	static {
+        Field f = null;
+        try {
+            f = Fragment.class.getDeclaredField("mChildFragmentManager");
+            f.setAccessible(true);
+        }
+        catch (NoSuchFieldException e) {
+            Log.e(TAG, "Error getting mChildFragmentManager field", e);
+        }
+        sChildFragmentManagerField = f;
+    }
+    @Override
+    public void onDetach() {
+        super.onDetach();
+
+        if (sChildFragmentManagerField != null) {
+            try {
+                sChildFragmentManagerField.set(this, null);
+            } catch (Exception e) {
+                Log.e(TAG, "Error setting mChildFragmentManager field", e);
+            }
+        }
+    }
+    // end bug fix
+
 	private View view;
     private ViewPager viewPager;
 	private ProfilePagerAdapter pagerAdapter;
@@ -33,7 +66,30 @@ public class ProfileFragment extends Fragment {
 		Log.d(TAG, "onCreateView");
         view = inflater.inflate(R.layout.fragment_profile, container, false);
 
+        // create view pager (but don't bind adapter until activity is available)
         viewPager = (ViewPager) view.findViewById(R.id.profile_view_pager);
+
+        pagerAdapter = new ProfilePagerAdapter(getChildFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
+/*
+        // bind the title indicator to the view pager
+        UnderlinePageIndicator underlinePageIndicator = (UnderlinePageIndicator) view.findViewById(R.id.profile_titles);
+        underlinePageIndicator.setViewPager(viewPager);
+        underlinePageIndicator.setFades(false);
+        underlinePageIndicator.setOnPageChangeListener(new OnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+            }
+            
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+            
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+*/
         // temporary
         firstProfileFrag = new AboutMeFragment();
         secondProfileFrag = new MyItemsFragment();
@@ -44,9 +100,6 @@ public class ProfileFragment extends Fragment {
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 	    super.onActivityCreated(savedInstanceState);
-
-        pagerAdapter = new ProfilePagerAdapter(getActivity().getSupportFragmentManager());
-        viewPager.setAdapter(pagerAdapter);
 	}
 
 	/**
