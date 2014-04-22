876a28bd395c17379e93275bde4c4c24fd386a4f
Inez Korczyński <inez@wikia-inc.com>
2014-04-22 00:36:04 -0700

    Alternative model for messages and thread/conversations - based on http://www.9lessons.info/2013/05/message-conversation-database-design.html and not being used yet

---
+++ stuffapp/src/com/stuff/stuffapp/models/TempConversation.java
@@ -0,0 +1,38 @@
+package com.stuff.stuffapp.models;
+
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+@ParseClassName("TempConversation")
+public class TempConversation extends ParseObject  {
+
+	public TempConversation() {
+		super();
+	}
+	
+	public ParseUser getUserOne() {
+		return getParseUser("user_one");
+	}
+
+	public void setUserOne(ParseUser user) {
+		put("user_one", user);
+	}
+	
+	public ParseUser getUserTwo() {
+		return getParseUser("user_two");
+	}
+
+	public void setUserTwo(ParseUser user) {
+		put("user_two", user);
+	}
+
+	public Item getItem() {
+		return (Item) getParseObject("item");
+	}
+
+	public void setItem(Item item) {
+		put("item", item);
+	}
+
+}
