0b8be17cdd83c353ada6e5885a02d290ff43ac5d
Inez Korczyński <inez@wikia-inc.com>
2014-04-22 16:46:57 -0700

    Use createWithoutData as a workaround for dealing with serialized and then unserialized objects.

--- stuffapp/src/com/stuff/stuffapp/fragments/MessageComposeFragment.java
+++ stuffapp/src/com/stuff/stuffapp/fragments/MessageComposeFragment.java
@@ -1,27 +1,8 @@
 package com.stuff.stuffapp.fragments;
 
-import java.util.Calendar;
 import java.util.HashMap;
-import java.util.List;
-
-import com.parse.FindCallback;
-import com.parse.FunctionCallback;
-import com.parse.GetCallback;
-import com.parse.Parse;
-import com.parse.ParseCloud;
-import com.parse.ParseException;
-import com.parse.ParseInstallation;
-import com.parse.ParseQuery;
-import com.parse.ParseUser;
-import com.parse.SaveCallback;
-import com.stuff.stuffapp.R;
-import com.stuff.stuffapp.helpers.Helper;
-import com.stuff.stuffapp.models.Item;
-import com.stuff.stuffapp.models.Message;
-
 
 import android.os.Bundle;
-
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -32,6 +13,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import com.stuff.stuffapp.R;
+import com.stuff.stuffapp.helpers.Helper;
+import com.stuff.stuffapp.models.Item;
+import com.stuff.stuffapp.models.Message;
 
 public class MessageComposeFragment extends Fragment{
 	
@@ -74,56 +64,20 @@ public class MessageComposeFragment extends Fragment{
 		btSend.setOnClickListener(new OnClickListener(){			
 			@Override
 			public void onClick(View v) {
-				
 				Toast.makeText(getActivity(), "Sending", Toast.LENGTH_LONG).show();
 
-	/* This does NOT work			
-				Message message = new Message();
-				message.setText(etCompose.getText().toString());
-				message.setFromUser(getLoggedInUser());
-				message.setToUser(mForItem.getOwner());
-			    message.setItem(mForItem);
-				Log.d(TAG,getLoggedInUser().getObjectId());
-				message.saveInBackground(new SaveCallback(){
-
+				com.stuff.stuffapp.models.Message m = new com.stuff.stuffapp.models.Message();
+				m.setText(etCompose.getText().toString());
+				m.setFromUser(ParseUser.getCurrentUser());
+				m.setToUser(ParseObject.createWithoutData(ParseUser.class, mForItem.getOwner().getObjectId()));
+				m.setItem(ParseObject.createWithoutData(Item.class, mForItem.getObjectId()));
+				m.saveInBackground(new SaveCallback() {
 					@Override
-					public void done(ParseException ex) {
-						// TODO Auto-generated method stub
-						if(ex != null) {
-							ex.printStackTrace();
-						}
+					public void done(ParseException arg0) {
+						Log.d(TAG, "Save Done: " + ( arg0 != null ? arg0.getMessage() : ""));
 					}
-					
-				}); */
-				
-	
-// This works! Read the item (for the owner) first (even though you already have the item and the user	) 	
-		 ParseQuery<Item> query = ParseQuery.getQuery(Item.class);
-		 query.getInBackground(mForItem.getObjectId(), new GetCallback<Item>() {
-		     public void done(Item item, ParseException ex) {
-		         if (ex == null) {
-		        	 
-		        	 Log.d(TAG, item.getObjectId());
-						com.stuff.stuffapp.models.Message m = new com.stuff.stuffapp.models.Message();
-						m.setText(etCompose.getText().toString());
-						m.setFromUser(ParseUser.getCurrentUser());
-						m.setToUser(item.getOwner());
-						m.setItem(item);
-						m.saveInBackground();
-		             
-		         } else {
-		             
-						Log.d(TAG, "Error retrieving object : " + ex.toString());
-						ex.printStackTrace();
-		         }
-		     }
-		 });
-		 
-		
-
-				
+				});
 			}
-			
 		});
         
 		return view;
