546f8cbc7a83137e424227259b3d17f2721344e4
Inez Korczyński <inez@wikia-inc.com>
2014-04-25 19:57:24 -0700

    Format code

--- stuffapp/src/com/stuff/stuffapp/fragments/DetailsFragment.java
+++ stuffapp/src/com/stuff/stuffapp/fragments/DetailsFragment.java
@@ -26,7 +26,7 @@ public class DetailsFragment extends Fragment {
 	private static String TAG = "DetailsFragment";
 
 	private View view;
-	
+
 	private Item item;
 
 	public static DetailsFragment newInstance(Item item) {
@@ -36,55 +36,55 @@ public class DetailsFragment extends Fragment {
 		fragment.setArguments(bundle);
 		return fragment;
 	}
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		Bundle args = getArguments();
 		item = (Item) args.getSerializable("item");
 	}
-	
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        Log.d(TAG, "onCreateView: " + item.getName());
-        view = inflater.inflate(R.layout.fragment_details, container, false);
+		Log.d(TAG, "onCreateView: " + item.getName());
+		view = inflater.inflate(R.layout.fragment_details, container, false);
 
-        //
-        // tvName
-        //
-        TextView tvName = (TextView) view.findViewById(R.id.tvName);
-        tvName.setText(item.getName());
-        
-        //
-        // ivItemPicture
-        //
-        ParseImageView ivItemPicture = (ParseImageView) view.findViewById(R.id.ivItemPicture);
-        ImageLoader imageLoader = ImageLoader.getInstance();
-        imageLoader.displayImage(item.getPhotoFile().getUrl(), ivItemPicture);
+		//
+		// tvName
+		//
+		TextView tvName = (TextView) view.findViewById(R.id.tvName);
+		tvName.setText(item.getName());
+
+		//
+		// ivItemPicture
+		//
+		ParseImageView ivItemPicture = (ParseImageView) view.findViewById(R.id.ivItemPicture);
+		ImageLoader imageLoader = ImageLoader.getInstance();
+		imageLoader.displayImage(item.getPhotoFile().getUrl(), ivItemPicture);
 
 		//
 		// tvDistance
 		//
 		ParseGeoPoint itemLocation = item.getLocation();
 		ParseGeoPoint userLocation = ((MainActivity) getActivity()).getLastKnownLocation();
-        TextView tvDistance = (TextView) view.findViewById(R.id.tvDistance);
+		TextView tvDistance = (TextView) view.findViewById(R.id.tvDistance);
 		if (itemLocation != null && userLocation != null) {
 			double distanceToItem = userLocation.distanceInMilesTo(itemLocation);
 			tvDistance.setText(String.format("%.1f miles", distanceToItem));
 		}
-		
+
 		//
 		// tvTime
 		//
 		TextView tvTime = (TextView) view.findViewById(R.id.tvTime);
 		tvTime.setText((new PrettyTime()).format(item.getCreatedAt()));
-		
+
 		//
 		// tvDescription
 		//
 		TextView tvDescription = (TextView) view.findViewById(R.id.tvDescription);
 		String description = item.getDescription();
-		if(description.trim().length() == 0) {
+		if (description.trim().length() == 0) {
 			description = "<i>No description</i>";
 		}
 		tvDescription.setText(Html.fromHtml(description));
@@ -96,7 +96,8 @@ public class DetailsFragment extends Fragment {
 		//
 		ImageView ivProfilePicture = (ImageView) view.findViewById(R.id.ivProfilePicture);
 		try {
-			imageLoader.displayImage("http://graph.facebook.com/" + profileData.get("facebookId").toString() + "/picture?type=normal", ivProfilePicture);
+			imageLoader.displayImage("http://graph.facebook.com/" + profileData.get("facebookId").toString()
+					+ "/picture?type=normal", ivProfilePicture);
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
@@ -117,7 +118,7 @@ public class DetailsFragment extends Fragment {
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
-		
+
 		tvNameAndLocation.setText(name + (location != null ? "\n" + location : ""));
 
 		return view;
